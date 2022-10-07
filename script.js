401eedb5a0091ec0f13ce66be004948d9c8d422c
alysvolatile <alice.g.cooper@gmail.com>
2022-10-07 13:57:30 -0400

    refactored js to fetch instead of ajax

--- script.js
+++ script.js
@@ -4,18 +4,33 @@ button.addEventListener("click", getDog);
 
 function getDog(e) {
     e.preventDefault();
-    const http = new XMLHttpRequest();
-    http.open("GET", "https://dog.ceo/api/breeds/image/random", true)
-    http.onload = function() {
-        if (this.status === 200) {
-            const dog = JSON.parse(this.responseText)
-            let output = `
+    // ajax style
+        // const http = new XMLHttpRequest();
+        // http.open("GET", "https://dog.ceo/api/breeds/image/random", true)
+        // http.onload = function() {
+        //     if (this.status === 200) {
+        //         const dog = JSON.parse(this.responseText)
+        //         dogDisplay.innerHTML = `
+        //             <img src=${dog.message} />
+        //         `;
+        //     } else {
+        //         dogDisplay.innerHTML = `<h3>Oops, there was an error! Try again!</h3>`
+        //     }
+        // }
+        // http.send();
+
+    //fetch style
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(dog) {
+            dogDisplay.innerHTML = `
                 <img src=${dog.message} />
             `;
-            dogDisplay.innerHTML = output;
-        } else {
-            let output = `<h3>Oops, there was an error! Try again!</h3>`
-        }
-    }
-    http.send();
+        })
+        .catch((error) => {
+            console.log('error')
+            dogDisplay.innerHTML = `<h3>Oops, there was an error! Try again!</h3>`
+        })
 }
