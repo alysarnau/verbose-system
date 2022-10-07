7326089dcc98bba1d690d939e1e797e8ee304b5c
alysvolatile <alice.g.cooper@gmail.com>
2022-10-07 13:57:53 -0400

    streamlined code

--- script.js
+++ script.js
@@ -4,22 +4,6 @@ button.addEventListener("click", getDog);
 
 function getDog(e) {
     e.preventDefault();
-    // ajax style
-        // const http = new XMLHttpRequest();
-        // http.open("GET", "https://dog.ceo/api/breeds/image/random", true)
-        // http.onload = function() {
-        //     if (this.status === 200) {
-        //         const dog = JSON.parse(this.responseText)
-        //         dogDisplay.innerHTML = `
-        //             <img src=${dog.message} />
-        //         `;
-        //     } else {
-        //         dogDisplay.innerHTML = `<h3>Oops, there was an error! Try again!</h3>`
-        //     }
-        // }
-        // http.send();
-
-    //fetch style
     fetch("https://dog.ceo/api/breeds/image/random")
         .then(function(response) {
             return response.json();
@@ -30,7 +14,6 @@ function getDog(e) {
             `;
         })
         .catch((error) => {
-            console.log('error')
             dogDisplay.innerHTML = `<h3>Oops, there was an error! Try again!</h3>`
         })
 }
